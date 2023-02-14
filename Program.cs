0b803749469c0db252c812146cc2efb00c4823cd
L1LCH1LL <108305081+L1LCH1LL@users.noreply.github.com>
2023-02-15 01:51:39 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,35 +1,49 @@
-﻿//Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
 
 Console.Clear();
-Console.WriteLine($"Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.");
-Console.WriteLine($"\n Введите размер массива m x n и диапазон случайных значений:");
-int m = InputNumbers("Введите m: ");
-int n = InputNumbers("Введите n: ");
-int range = InputNumbers("Введите диапазон: от 1 до ");
+Console.WriteLine($"Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.\n\n Сразу зададим матрицу");
+Console.WriteLine($"\n Введите размеры матриц и диапазон случайных значений:");
+int m = InputNumbers("Введите число строк 1-й матрицы: ");
+int n = InputNumbers("Введите число столбцов 1-й матрицы (и строк 2-й): ");
+int p = InputNumbers("Введите число столбцов 2-й матрицы: ");
+int range = InputNumbers("Введите диапазон случайных чисел: от 1 до ");
 
-int[,] array = new int[m, n];
-CreateArray(array);
-WriteArray(array);
+int[,] firstMartrix = new int[m, n];         //первая матрица
+CreateArray(firstMartrix);
+Console.WriteLine($"\n Первая матрица:");
+WriteArray(firstMartrix);
 
-Console.WriteLine($"\n Отсортированный массив: ");
-OArrayLines(array);
-WriteArray(array);
 
-void OArrayLines(int[,] array)                     //основная часть (распределение)
+int[,] secomdMartrix = new int[n, p];          //вторая матрица
+CreateArray(secomdMartrix);
+Console.WriteLine($"\n Вторая матрица:");
+WriteArray(secomdMartrix);
+
+
+
+int[,] resultMatrix = new int[m,p];            // результат перемножения
+
+
+
+MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);    // вывод произведения
+Console.WriteLine($"\n Произведение первой и второй матриц:");
+WriteArray(resultMatrix);
+
+
+
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)  // перемножение
 {
-  for (int i = 0; i < array.GetLength(0); i++)
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
   {
-    for (int j = 0; j < array.GetLength(1); j++)
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
     {
-      for (int k = 0; k < array.GetLength(1) - 1; k++)  
+      int sum = 0;
+      for (int k = 0; k < firstMartrix.GetLength(1); k++)
       {
-        if (array[i, k] < array[i, k + 1])
-        {
-          int temp = array[i, k + 1];
-          array[i, k + 1] = array[i, k];
-          array[i, k] = temp;
-        }
+        sum += firstMartrix[i,k] * secomdMartrix[k,j];
       }
+      resultMatrix[i,j] = sum;
     }
   }
 }
@@ -37,12 +51,15 @@ void OArrayLines(int[,] array)                     //основная часть
 
 
 
-int InputNumbers(string input)                          // Создание матрицы
+int InputNumbers(string input)
 {
   Console.Write(input);
   int output = Convert.ToInt32(Console.ReadLine());
   return output;
 }
+
+
+////////////////////////////// создание и вывод матрицы 
 
 void CreateArray(int[,] array)
 {
@@ -58,14 +75,16 @@ void CreateArray(int[,] array)
 
 
 
-void WriteArray(int[,] array)                           // Вывод матрицы
+
+void WriteArray (int[,] array)
 {
   for (int i = 0; i < array.GetLength(0); i++)
   {
     for (int j = 0; j < array.GetLength(1); j++)
     {
-      Console.Write(array[i, j] + " ");
+      Console.Write(array[i,j] + " ");
     }
     Console.WriteLine();
   }
 }
+
